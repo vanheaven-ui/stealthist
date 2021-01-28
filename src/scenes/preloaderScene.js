7932a7280d14b83c6bd5c061345de42816ae8af8
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-01-28 18:07:24 +0300

    Add relevant boiler scene files

--- src/scenes/preloaderScene.js
+++ src/scenes/preloaderScene.js
@@ -9,6 +9,11 @@ export default class PreloaderScene extends Phaser.Scene {
   preload() {
     this.add.image(CST.dimens(this).width / 2, CST.dimens(this).height / 2, 'logo')
     this.load.image('background', 'assets/images/title_bg.jpg');
+    this.load.spritesheet('dude', 'assets/images/male.png', { frameWidth: 32, frameHeight: 32 });
+    this.load.image('tiles', 'assets/images/RPG Nature Tileset.png');
+    this.load.image('play', 'assets/images/play_button.png');
+    this.load.image('options', 'assets/images/options_button.png');
+
 
     this.loadingBar = this.add.graphics();
     this.loadingBox = this.add.graphics();
--- src/scenes/titleScene.js
+++ src/scenes/titleScene.js
@@ -9,5 +9,7 @@ export default class TitleScene extends Phaser.Scene {
   create() {
     this.add.image(CST.dimens(this).width / 2 * 1.25, CST.dimens(this).height / 2 * 0.30, 'logo').setDepth(1);
     this.add.image(0, 0, 'background').setOrigin(0, 0);
+    this.add.image(CST.dimens(this).width / 2, CST.dimens(this).height / 2 * 1.30, 'options')
+    this.add.image(CST.dimens(this).width / 2, CST.dimens(this).height / 2, 'play').setDepth(2);
   }
 }
