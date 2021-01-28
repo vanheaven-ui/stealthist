2e81ccb721566ba2063c34cd79b3565ed993f0f9
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-01-29 01:25:24 +0300

    Resolve linter offenses

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,10 @@
 const { merge } = require('webpack-merge');
- const common = require('./webpack.common.js');
+const common = require('./webpack.common.js');
 
- module.exports = merge(common, {
-   mode: 'development',
-   devtool: 'inline-source-map',
-   devServer: {
-     contentBase: './dist',
-   },
- });
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+});
