8e08cefbaec185595a3ac8dd285c67082d0d0c19
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-01-29 22:44:28 +0300

    Update player.js class

--- src/characters/player.js
+++ src/characters/player.js
@@ -5,12 +5,30 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
   constructor (scene, x, y, texture, frame) {
     super(scene, x, y, texture, frame);
 
-    //  You can either do this:
     scene.add.existing(this);
     scene.physics.add.existing(this);
+
     this.setScale(1.5);
     this.setVelocity(0);
     this.setCollideWorldBounds(true);
     this.setVelocity(0);
-  }  
+  }
+  
+  movePlayer() {
+    this.setVelocity(0);
+    
+    if (this.cursors.left.isDown) {
+      this.setVelocityX(-160);
+    }
+    else if (this.cursors.right.isDown) {
+      this.setVelocityX(160);
+    }
+
+    if (this.cursors.up.isDown) {
+      this.setVelocityY(-160);
+    }
+    else if (this.cursors.down.isDown) {
+      this.setVelocityY(160);
+    }
+  }
 }
