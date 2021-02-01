6aaad9d380f675cd811497406176b959d0361537
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-02-01 11:09:12 +0300

    Add Menu class

---
+++ src/utils/menu.js
@@ -0,0 +1,19 @@
+import CST from './utils';
+
+export default class Menu {
+  static createMenuBtn(scene) {
+    scene.menuBtn = scene.add.image(
+      CST.dimens(scene).width / 2 - 200,
+      CST.dimens(scene).height * 0.9,
+      'menu',
+    );
+    
+    scene.menuBtn.setInteractive();
+    
+    scene.menuBtn.on('pointerdown', () => {
+      scene.scene.start(CST.scenes.TITLE);
+    });
+  }
+  
+}
+
