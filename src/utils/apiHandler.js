bc18b4939ce5aa9e1441e605f3180cc51547bacf
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-02-02 16:18:45 +0300

    Update apiHander Module

--- src/utils/apiHandler.js
+++ src/utils/apiHandler.js
@@ -1,29 +1,25 @@
 const APIHandler = (() => {
   const postEndPoint = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
-  const getEndPoint = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+  const baseGETEndPoint = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/CdISo1zrmscAtAtqHEmn/scores';
   const gameData = { name: 'stealthist' };
-  const initData = {
-    mode: 'cors',
-    method: 'POST',
-    header: {
-      COntent_Type: 'application/json',
-    },
-    body: JSON.stringify(data)
-  }
  
-  const postData = async(Uurl, data) => {
-    const response = await fetch(url, initData)
-    .then(res => res.json())
-    .then(data => consol.log(data));
-
-    return response;
+  const postData = async(url, data) => {
+    const response = await fetch(url, {
+      mode: 'cors',
+      method: 'POST',
+      headers: {
+        COntent_Type: 'application/json',
+      },
+      body: JSON.stringify(data)
+    })
+    return response.json();
   };
 
   const getData = async(url) => {
-    const response = await fetch(url)
-    .then(res => res.json())
-    .then(data => console.log(data));
-  };
+    const response = await (await fetch(url));
+  
+    return response.json();
+  }
 
   return { postData, getData };
 })();
