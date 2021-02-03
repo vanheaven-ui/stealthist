edbac7bd7579ae72561d5d44945ae9b8aceb5a56
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-02-04 00:42:39 +0300

    Add mesages to failScene

--- src/scenes/failScene.js
+++ src/scenes/failScene.js
@@ -1,6 +1,7 @@
 import Phaser from 'phaser';
 import { CST } from '../utils/utils';
 import Menu from '../utils/menu';
+import GameScene from './gameScene';
 
 export default class FailScene extends Phaser.Scene {
   constructor() {
@@ -11,6 +12,17 @@ export default class FailScene extends Phaser.Scene {
     this.add.image(CST.dimens(this).width / 2, 100, 'gameoverTitle');
     const restartBtn = this.add.image((CST.dimens(this).width / 2) * 1.50, 400, 'restart');
     this.quitBtn = this.add.image(CST.dimens(this).width / 2, 480, 'quit');
+
+    GameScene.trackGameFailState();
+
+    if (CST.state.forageFail) {
+      this.cameras.main.shake(100);
+      this.add.image(250, 300, 'timeout').setAngle(-15);
+    }
+    else {
+      this.cameras.main.shake(100);
+      this.add.image(250, 300, 'close').setAngle(-15);
+    }
     
     [restartBtn, this.quitBtn].forEach(btn => btn.setInteractive());
 
