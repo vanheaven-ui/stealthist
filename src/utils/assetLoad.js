95ab95cd6dfe99e690b9cec8fe6fa17f8a147a42
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-02-03 18:43:17 +0300

    Load more game assets

--- src/utils/assetLoad.js
+++ src/utils/assetLoad.js
@@ -24,10 +24,42 @@ const MyModule = (() => {
     scene.load.image('disable-music', 'assets/images/disable.png');
     scene.load.image('enabled', 'assets/images/musicEnabled.png');
     scene.load.image('disabled', 'assets/images/musicDisabled.png');
+    
     scene.load.audio('gameSound', 'assets/audio/game.mp3');
+    scene.load.audio('collect', 'assets/audio/collectitem.mp3');
+    scene.load.audio('game-over', 'assets/audio/gameover.mp3');
     
     scene.load.spritesheet('dude4', 'assets/sprites/ezekiel.png', { frameWidth: 60, frameHeight: 70 });
     scene.load.animation('dude_anim', 'assets/sprites/dude1_anim.json');
+
+    scene.load.image('options-title', 'assets/images/optionsTitle.png');
+    scene.load.image('options-title1', 'assets/images/optionsTitle1.png');
+
+    scene.load.image('tiles1', 'assets/images/DungeonTileset.png');
+    scene.load.tilemapTiledJSON('map1', 'assets/map/dungeon.json');
+
+    scene.load.image('tiles3', 'assets/images/Dungeon_Tileset_at.png');
+    scene.load.tilemapTiledJSON('map2', 'assets/map/dungeons.json');
+
+    scene.load.image('gameoverTitle', 'assets/images/gameover.png');
+
+    scene.load.image('restart', 'assets/images/restart.png');
+
+    scene.load.image('quit', 'assets/images/quit.png');
+
+    scene.load.image('best-score', 'assets/images/bestscore.png');
+
+    scene.load.image('welldone', 'assets/images/welldone.png');
+
+    scene.load.image('player-score', 'assets/images/timescore.png');
+
+    scene.load.image('treasure', 'assets/images/treasure.png');
+
+    scene.load.image('player', 'assets/images/player.png');
+
+    scene.load.image('play-again', 'assets/images/play-again.png');
+
+    scene.load.image('yourtime', 'assets/images/yourtime.png');
   }
   return { assetLoad }
 })();
